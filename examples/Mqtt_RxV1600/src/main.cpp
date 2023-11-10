1e89b65be5cf649fb4865913856e727629df1a96
Joachim Banzhaf <Joachim.Banzhaf@gmail.com>
2023-11-10 01:05:27 +0100

    use iso date for compile time on web page

--- examples/Mqtt_RxV1600/src/main.cpp
+++ examples/Mqtt_RxV1600/src/main.cpp
@@ -55,6 +55,26 @@ RxV1600 rxv;
 auto help = RxV1600::end();
 
 
+// define constant IsoDate as nicer variant of __DATE__ (from https://stackoverflow.com/a/64718070)
+constexpr unsigned int compileYear = (__DATE__[7] - '0') * 1000 + (__DATE__[8] - '0') * 100 + (__DATE__[9] - '0') * 10 + (__DATE__[10] - '0');
+constexpr unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
+                                : (__DATE__[0] == 'F') ? 2                                                              // Feb
+                                : (__DATE__[0] == 'M') ? ((__DATE__[2] == 'r') ? 3 : 5)                                 // Mar or May
+                                : (__DATE__[0] == 'A') ? ((__DATE__[2] == 'p') ? 4 : 8)                                 // Apr or Aug
+                                : (__DATE__[0] == 'S') ? 9                                                              // Sep
+                                : (__DATE__[0] == 'O') ? 10                                                             // Oct
+                                : (__DATE__[0] == 'N') ? 11                                                             // Nov
+                                : (__DATE__[0] == 'D') ? 12                                                             // Dec
+                                : 0;
+constexpr unsigned int compileDay = (__DATE__[4] == ' ') ? (__DATE__[5] - '0') : (__DATE__[4] - '0') * 10 + (__DATE__[5] - '0');
+constexpr char IsoDate[] = {
+    compileYear/1000 + '0', (compileYear % 1000)/100 + '0', (compileYear % 100)/10 + '0', compileYear % 10 + '0',
+    '-',  compileMonth/10 + '0', compileMonth%10 + '0',
+    '-',  compileDay/10 + '0', compileDay%10 + '0',
+    0
+};
+
+
 void slog(const char *message, uint16_t pri = LOG_INFO) {
     static bool log_infos = true;
     
@@ -160,7 +180,7 @@ const char *main_page() {
         "   </tr>\n"
         "   <tr><td>Startzeit</td><td>%s</td><td></td></tr>\n"
         "   <tr><td>Ladezeit</td><td>%s</td><td></td></tr>\n"
-        "   <tr><td>Update URL</td><td><a href=\"http://" HOSTNAME "/update\">Update</a></td><td></td></tr>\n"
+        "   <tr><td>Version</td><td>" VERSION " %s " __TIME__ "</td><td><a href=\"http://" HOSTNAME "/update\">Update</a></td></tr>\n"
         "   <tr><td>ESP</td><td><form method=\"POST\" action=\"/reset\"><input type=\"submit\" value=\"Reset\"></form></td><td></td></tr>\n"
         "  </table>\n"
         " </body>\n"
@@ -182,7 +202,7 @@ const char *main_page() {
         muted ? muted : "unknown",
         volume ? volume : "unknown",
         input ? input : "unknown",
-        start_time, curr_time);
+        start_time, curr_time, IsoDate);
     *web_msg = '\0';
     return page;
 }
